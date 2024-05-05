e4792351b6b44d7ccb8d1d7053282b756156f96e
utkarsharsh <harshupadhyay7786@gmail.com>
2024-05-05 09:33:42 +0530

    All done

--- Tapop/src/auth/auth.js
+++ Tapop/src/auth/auth.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+const api=import.meta.env.VITE_API
+const project=import.meta.env.VITE_PROJECT
 const firebaseConfig = {
-    apiKey: "AIzaSyBPKmzI_OKTt0ElsCJ2VlWBFjoYsTQaaP8",
-    projectId: "tapop-e8824"
+    apiKey: api,
+    projectId: project
   };
   
-  // Initialize Firebase
+  
   const app = initializeApp(firebaseConfig);
   
   
-  // Initialize Firebase Authentication and get a reference to the service
  export const auth = getAuth(app);
